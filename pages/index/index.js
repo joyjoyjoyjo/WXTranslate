ed748737615dbf7f55fde97bf8dc21e52347452b
xiaoyiyi <xiaoyishikongde@163.com>
2019-02-02 23:46:32 +0800

    Index页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,69 +1,52 @@
-// pages/history/history.js
+//index.js
+//获取应用实例
+import { translate } from '../../utils/api.js'
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    query:'',
-    hideClearIcon:true,
-    result:[],
-    curLang:{}
+    query: '',
+    hideClearIcon: true,
+    result: [],
+    curLang: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    console.log('lonload..')
+    console.log(options)
+    if (options.query) {
+      this.setData({ query: options.query })
+    }
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+    if (this.data.curLang.lang !== app.globalData.curLang.lang) {
+      this.setData({ curLang: app.globalData.curLang })
+      this.onConfirm()
+    }
 
   },
+  onInput: function (e) {
+    this.setData({ 'query': e.detail.value })
+    if (this.data.query.length > 0) {
+      this.setData({ 'hideClearIcon': false })
+    } else {
+      this.setData({ 'hideClearIcon': true })
+    }
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+    console.log('focus')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onTapClose: function () {
+    this.setData({ query: '', hideClearIcon: true })
   },
+  onConfirm: function () {
+    if (!this.data.query) return
+    translate(this.data.query, { from: 'auto', to: this.data.curLang.lang }).then(res => {
+      this.setData({ 'result': res.trans_result })
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      let history = wx.getStorageSync('history') || []
+      history.unshift({ query: this.data.query, result: res.trans_result[0].dst })
+      history.length = history.length > 10 ? 10 : history.length
+      wx.setStorageSync('history', history)
+    })
   }
 })
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,13 @@
+import md5 from './md5.js'
+
+const appid = '20190131000261222'
+const key = 'Ej37vEQR8G1A8DVtyXso'
+
+
 function translate(q, { from='auto', to='auto'}= {from:'auto',to:'auto'}){
   return new Promise((resolve,reject) =>{
     let salt = Data.now()
-    let sign = md5(`${20190131000261222}${q}${salt}${Ej37vEQR8G1A8DVtyXso}`)
+    let sign = md5(`${appid}${q}${salt}${key}`)
     wx.request({
       url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
       data:{
@@ -12,7 +18,27 @@ function translate(q, { from='auto', to='auto'}= {from:'auto',to:'auto'}){
         salt,
         sign
       },
-      success(res)
+      success(res){
+        if(res.data && res.data.trans_reslut) {
+          resolve(res.data)
+        }else{
+          reject({ status:'error',msg:'翻译失败'})
+          wx.showToast({
+            title: '翻译失败',
+            icon:'none',
+            duration:3000
+          })
+        }
+      },
+      fail(){
+        reject({status:'error',msg:'翻译失败'})
+        wx.showToast({
+          title: '网络异常',
+          icon:'none',
+          duration:3000
+        })
+      }
     })
   })
 }
+module.exports.translate = translate
