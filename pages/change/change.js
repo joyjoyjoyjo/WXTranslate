25e54ebd3cde40ede2cc62f4229f273b159a4576
xiaoyiyi <xiaoyishikongde@163.com>
2019-02-10 01:32:23 +0800

    翻译页面

--- pages/change/change.js
+++ pages/change/change.js
@@ -1,11 +1,49 @@
-const util = require('../../utils/util.js)
+const util = require('../../utils/util.js')
 const app = getApp()
 Page({
   data: {
     curLang:{},
-    langList:app.gloabalData.langList
+    langList: [
+      {
+        'chs': '英文',
+        "lang": 'en',
+        "index": 0
+      },
+      {
+        'chs': '中文',
+        'lang': 'zh',
+        "index": 1
+      },
+      {
+        'chs': '日语',
+        'lang': 'jp',
+        "index": 2
+      },
+      {
+        'chs': '韩语',
+        'lang': 'kor',
+        "index": 3
+      },
+      {
+        'chs': '法语',
+        'lang': 'fra',
+        "index": 4
+      },
+      {
+        'chs': '西班牙语',
+        'lang': 'spa',
+        "index": 5
+      },
+      {
+        'chs': '阿拉伯语',
+        'lang': 'ara',
+        "index": 6
+      }
+    ]
   },
-
+  onShow: function () {
+    this.setData({ curLang: app.globalData.curLang })
+  },
   onTapItem: function (e) {
     let langObj = e.currentTarget.dataset
     wx.setStorageSync('language', langObj)
--- utils/api.js
+++ utils/api.js
@@ -1,16 +1,16 @@
-import md5 from './md5.js'
+import md5 from './md5.min.js'
 
 const appid = '20190131000261222'
 const key = 'Ej37vEQR8G1A8DVtyXso'
 
 
-function translate(q, { from='auto', to='auto'}= {from:'auto',to:'auto'}){
-  return new Promise((resolve,reject) =>{
-    let salt = Data.now()
+function translate(q, { from = 'auto', to = 'auto' } = { from: 'auto', to: 'auto' }) {
+  return new Promise((resolve, reject) => {
+    let salt = Date.now()
     let sign = md5(`${appid}${q}${salt}${key}`)
     wx.request({
       url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-      data:{
+      data: {
         q,
         from,
         to,
@@ -18,24 +18,24 @@ function translate(q, { from='auto', to='auto'}= {from:'auto',to:'auto'}){
         salt,
         sign
       },
-      success(res){
-        if(res.data && res.data.trans_reslut) {
+      success(res) {
+        if (res.data && res.data.trans_result) {
           resolve(res.data)
-        }else{
-          reject({ status:'error',msg:'翻译失败'})
+        } else {
+          reject({ status: 'error', msg: '翻译失败' })
           wx.showToast({
             title: '翻译失败',
-            icon:'none',
-            duration:3000
+            icon: 'none',
+            duration: 3000
           })
         }
       },
-      fail(){
-        reject({status:'error',msg:'翻译失败'})
+      fail() {
+        reject({ status: 'error', msg: '翻译失败' })
         wx.showToast({
           title: '网络异常',
-          icon:'none',
-          duration:3000
+          icon: 'none',
+          duration: 3000
         })
       }
     })
